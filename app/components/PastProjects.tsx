91d44b9f3c426372f10e35c3310e65749ecdcb48
BestinMW <bestin.musashi.watts@gmail.com>
2026-08-17 14:31:06 -0700

    added pastprojects data file & tweaked var names

--- app/components/PastProjects.tsx
+++ app/components/PastProjects.tsx
@@ -1,54 +1,9 @@
 "use client";
 import { useState} from "react";
 import Image from "next/image";
+import {Project, PROJECTS} from "../data/pastprojects";
 
-
-
-type PROJECT = {
-    title: string;
-    description: string;
-    image: string;
-    link: string;
-    testimonial: string;
-    track: string;
-    hackathon: string;
-    year: string;
-};
-
-const projects: PROJECT[] = [
-    {
-        title: "Project example",
-        description: "desc",
-        image: "/example.svg",
-        link: "example.com",
-        testimonial: "something",
-        track: "Best Overall",
-        hackathon: "Citrus Hack",
-        year: "3036",
-    },
-    {
-        title: "Project example 2",
-        description: "desc 2",
-        image: "/example2.png",
-        link: "example2.com",
-        testimonial: "something something",
-        track: "Beginner Hack",
-        hackathon: "Citrus Hack",
-        year: "2027",
-    },
-    {
-        title: "Project example 3",
-        description: "desc 3",
-        image: "/example3.png",
-        link: "example3.com",
-        testimonial: "something something something",
-        track: "Hardware",
-        hackathon: "Cutie Hack",
-        year: "1300",
-    },
-];
-
-function PastProjectBubble({project, onClick, className}: {project: PROJECT; onClick: () => void; className: string;}) {
+function PastProjectBubble({project, onClick, className}: {project: Project; onClick: () => void; className: string;}) {
     return (
         <button type="button" onClick={onClick} className={`overflow-hidden group absolute items-center justify-center bg-gray-300 ${className}`}>
                 <span className="text-s text-center transition-opacity group-hover:opacity-0">
@@ -66,9 +21,9 @@ function PastProjectBubble({project, onClick, className}: {project: PROJECT; onC
 
 const PastProjects = () => {
     const [curIndex, setIndex] = useState(0);
-    const curProject = projects[curIndex];
-    const prevProj = () => setIndex((i) => (i-1+projects.length) % projects.length);
-    const nextProj = () => setIndex((i) => (i+1) % projects.length);
+    const curProject = PROJECTS[curIndex];
+    const prevProj = () => setIndex((i) => (i-1+PROJECTS.length) % PROJECTS.length);
+    const nextProj = () => setIndex((i) => (i+1) % PROJECTS.length);
 
     return (
 
@@ -79,13 +34,13 @@ const PastProjects = () => {
             {/*past project bubbles*/}
             <div className="relative max-w-xl mx-auto">
             {/*top right*/}
-            <PastProjectBubble project={projects[0]} onClick={() => setIndex(0)} className="-left-50 top-20 rounded-full h-30 w-30" />
+            <PastProjectBubble project={PROJECTS[0]} onClick={() => setIndex(0)} className="-left-50 top-20 rounded-full h-30 w-30" />
             {/*bottom left*/}
-            <PastProjectBubble project={projects[1]} onClick={() => setIndex(1)} className="-left-60 top-70 rounded-full h-30 w-30" />
+            <PastProjectBubble project={PROJECTS[1]} onClick={() => setIndex(1)} className="-left-60 top-70 rounded-full h-30 w-30" />
             {/*top right*/}
-            <PastProjectBubble project={projects[2]} onClick={() => setIndex(2)} className="-right-50 top-20 rounded-full h-30 w-30" />
+            <PastProjectBubble project={PROJECTS[2]} onClick={() => setIndex(2)} className="-right-50 top-20 rounded-full h-30 w-30" />
             {/*bottom right*/}
-            <PastProjectBubble project={projects[2]} onClick={/*change to more past project link or smthin*/() => setIndex(2)} className="-right-50 top-70 rounded-2xl h-25 w-40" />
+            <PastProjectBubble project={PROJECTS[2]} onClick={/*change to more past project link or smthin*/() => setIndex(2)} className="-right-50 top-70 rounded-2xl h-25 w-40" />
             </div>
 
 
@@ -105,7 +60,7 @@ const PastProjects = () => {
                     <div className="mt-10 flex flex-row items-center justify-center gap-4">
                         <button onClick={prevProj}>{"<"}</button>
                         <div className="flex items-center gap-4">
-                        {projects.map((_, i) => (
+                        {PROJECTS.map((_, i) => (
                             <button
                             key={i}
                             onClick={() => setIndex(i)}
