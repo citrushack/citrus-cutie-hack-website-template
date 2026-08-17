90e896e56dc724bac4cb0182e2d187541dd86b11
BestinMW <bestin.musashi.watts@gmail.com>
2026-08-17 14:35:12 -0700

    added data file & changed naming

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -1,76 +1,8 @@
 "use client";
 import {useState} from "react";
 import Image from "next/image";
+import {SPONSORS} from "../data/sponsors";
 
-type SponsorCard = {
-    id: string;
-    name: string;
-    description: string;
-    link: string;
-    image: string;
-};
-
-const sponsors: SponsorCard[] = [
-    {
-        id: "1",
-        name: "Google",
-        description: "Description 1",
-        link: "https://www.google.com",
-        image: "/file.svg",
-    },
-    {
-        id: "2",
-        name: "Amazon",
-        description: "Description 2",
-        link: "https://www.amazon.com",
-        image: "/images/amazon.png",
-    },
-    {
-        id: "3",
-        name: "Meta",
-        description: "Description 3",
-        link: "https://www.meta.com",
-        image: "/images/meta.png",
-    },
-    {
-        id: "4",
-        name: "Sponsor 4",
-        description: "Description 4",
-        link: "https://www.sponsor4.com",
-        image: "/images/sponsor4.png",
-    },
-    {
-        id: "5",
-        name: "Sponsor 5",
-        description: "Description 5",
-        link: "https://www.sponsor5.com",
-        image: "/images/sponsor5.png",
-    },
-    
-    {
-        id: "6",
-        name: "Sponsor 6",
-        description: "Description 6",
-        link: "https://www.sponsor6.com",
-        image: "/images/sponsor6.png",
-    },
-    {
-        id: "7",
-        name: "Sponsor 7",
-        description: "Description 7",
-        link: "https://www.sponsor7.com",
-        image: "/images/sponsor7.png",
-    },
-    {
-        id: "8",
-        name: "Sponsor 8",
-        description: "Description 8",
-        link: "https://www.sponsor8.com",
-        image: "/images/sponsor8.png",
-    },
-
-
-];
 
 
 
@@ -92,7 +24,7 @@ const Sponsors = () => {
                 Sponsors
             </h2>
             <div className="mx-auto grid max-w-4xl grid-cols-4 gap-4 mt-10">
-                {sponsors.map((sponsor)=>{
+                {SPONSORS.map((sponsor)=>{
                     const isFlipped = flippedIds.has(sponsor.id);
                     return(
                         <button
