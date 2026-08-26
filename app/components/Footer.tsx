7465ebd96cde8dd43b679946c8241e7f9929e7b5
BestinMW <bestin.musashi.watts@gmail.com>
2026-08-26 16:19:35 -0700

    added footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import {
+    FaInstagram,
+    FaDiscord,
+    FaLinkedin,
+    FaGithub,
+    FaRegEnvelope,
+} from "react-icons/fa";
+import Link from "next/link";
+const Footer = () => {
+  return (
+    <div className="relative flex items-center justify-center text-center">
+      <Link
+        href="https://www.instagram.com/instagram_user/"
+        target="_blank"
+        className="absolute bottom-[50%] left-[0%] z-40 hover:scale-105 hover:opacity-95"
+      >
+        <FaInstagram className="text-black text-4xl drop-shadow-xl md:text-6xl 2xl:text-8xl" />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/company/hackathon_name/"
+        target="_blank"
+        className="absolute bottom-[50%] left-[5%] z-40 hover:scale-105 hover:opacity-95"
+      >
+        <FaLinkedin className="text-4xl drop-shadow-xl md:text-6xl 2xl:text-8xl" />
+      </Link>
+      <Link
+        href="mailto:hackathon_name@gmail.com"
+        target="_blank"
+        className="absolute bottom-[50%] left-[10%] z-40 hover:scale-105 hover:opacity-95"
+      >
+        <FaRegEnvelope className="text-4xl md:text-6xl 2xl:text-8xl" />
+      </Link>
+      <p className="md:text-md absolute bottom-1 z-50 text-xs md:bottom-2 md:right-2 2xl:text-xl">{`© ${new Date().getFullYear()} (Name) Hack • Made with 💗 and (theme) by ACM Hacks`}</p>
+    </div>
+  );
+};
+
+export default Footer;
